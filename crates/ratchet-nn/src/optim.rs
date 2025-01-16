a395ca5922c7b55e40aacd2176f7712a19438a0f
Vin Howe <24789592+vinhowe@users.noreply.github.com>
2025-01-15 23:27:29 -0700

    Add AdamW optimizer

---
+++ crates/ratchet-nn/src/optim.rs
@@ -0,0 +1,200 @@
+#[cfg(target_arch = "wasm32")]
+use async_trait::async_trait;
+use half::{bf16, f16};
+use maybe_async::maybe_async;
+use ratchet::{DType, GradStore, Tensor, Var, WgpuDevice};
+
+#[maybe_async(AFIT)]
+#[cfg_attr(target_arch = "wasm32", async_trait::async_trait)]
+pub trait Optimizer: Sized {
+    type Config: Sized;
+
+    fn new(vars: Vec<(Option<String>, Var)>, config: Self::Config) -> anyhow::Result<Self>;
+
+    async fn step(&mut self, grads: &ratchet::GradStore) -> anyhow::Result<()>;
+
+    fn learning_rate(&self) -> f64;
+
+    fn set_learning_rate(&mut self, lr: f64);
+
+    fn empty(config: Self::Config) -> anyhow::Result<Self> {
+        Self::new(vec![], config)
+    }
+
+    fn schedule_backward_step(&mut self, loss: &Tensor) -> anyhow::Result<GradStore> {
+        loss.backward()
+    }
+
+    async fn backward_step(
+        &mut self,
+        grads: &mut GradStore,
+        device: &WgpuDevice,
+    ) -> anyhow::Result<()> {
+        grads.resolve(device).unwrap();
+        self.step(&grads).await
+    }
+
+    fn from_slice(vars: &[&Var], config: Self::Config) -> anyhow::Result<Self> {
+        let vars: Vec<_> = vars.iter().map(|&v| (None, v.clone())).collect();
+        Self::new(vars, config)
+    }
+}
+
+#[derive(Clone, Debug)]
+pub struct ParamsAdamW {
+    pub lr: f64,
+    pub beta1: f64,
+    pub beta2: f64,
+    pub eps: f64,
+    pub weight_decay: f64,
+}
+
+impl Default for ParamsAdamW {
+    fn default() -> Self {
+        Self {
+            lr: 0.001,
+            beta1: 0.9,
+            beta2: 0.999,
+            eps: 1e-8,
+            weight_decay: 0.01,
+        }
+    }
+}
+
+#[derive(Debug)]
+struct VarAdamW {
+    var: Var,
+    first_moment: Var,
+    second_moment: Var,
+    label: Option<String>,
+}
+
+#[derive(Debug)]
+pub struct AdamW {
+    vars: Vec<VarAdamW>,
+    step_t: usize,
+    params: ParamsAdamW,
+}
+
+#[maybe_async(AFIT)]
+#[cfg_attr(target_arch = "wasm32", async_trait::async_trait)]
+impl Optimizer for AdamW {
+    type Config = ParamsAdamW;
+
+    fn new(vars: Vec<(Option<String>, Var)>, params: ParamsAdamW) -> anyhow::Result<Self> {
+        let vars = vars
+            .into_iter()
+            .filter(|(_, var)| var.as_tensor().dt().is_float())
+            .map(|(label, var)| {
+                let var_t = var.as_tensor();
+                let dtype = var_t.dt();
+                let shape = var_t.shape();
+                let device = var_t.device();
+                let (first_moment, second_moment) = match dtype {
+                    DType::F32 => (
+                        Var::zeros::<f32>(shape, device),
+                        Var::zeros::<f32>(shape, device),
+                    ),
+                    DType::F16 => (
+                        Var::zeros::<f16>(shape, device),
+                        Var::zeros::<f16>(shape, device),
+                    ),
+                    DType::BF16 => (
+                        Var::zeros::<bf16>(shape, device),
+                        Var::zeros::<bf16>(shape, device),
+                    ),
+                    _ => return Err(anyhow::anyhow!("Unsupported dtype for AdamW: {:?}", dtype)),
+                };
+                Ok(VarAdamW {
+                    var,
+                    first_moment,
+                    second_moment,
+                    label,
+                })
+            })
+            .collect::<anyhow::Result<Vec<_>>>()?;
+        Ok(Self {
+            vars,
+            params,
+            step_t: 0,
+        })
+    }
+    fn learning_rate(&self) -> f64 {
+        self.params.lr
+    }
+
+    fn set_learning_rate(&mut self, lr: f64) {
+        self.params.lr = lr
+    }
+
+    async fn step(&mut self, grads: &ratchet::GradStore) -> anyhow::Result<()> {
+        self.step_t += 1;
+        let lr = self.params.lr;
+        let lambda = self.params.weight_decay;
+        let lr_lambda = lr * lambda;
+        let beta1 = self.params.beta1;
+        let beta2 = self.params.beta2;
+        let scale_m = 1f64 / (1f64 - beta1.powi(self.step_t as i32));
+        let scale_v = 1f64 / (1f64 - beta2.powi(self.step_t as i32));
+        for var in self.vars.iter_mut() {
+            let theta = &var.var;
+            let m = &var.first_moment;
+            let v = &var.second_moment;
+            let grad = grads.get(theta.as_tensor());
+
+            // println!("Optimizer stepping: {:?}", var.label);
+            // println!("Theta op: {:?}", theta.as_tensor().op());
+            // println!("Theta id: {:?}", theta.as_tensor().id());
+
+            if let Some(g) = grad {
+                let next_m = ((m.as_tensor().clone() * beta1 as f32)?
+                    + (g.clone() * (1.0 - beta1 as f32))?)?;
+                // let next_m = (m.as_tensor().clone() * beta1 as f32)?;
+                let next_v = ((v.as_tensor().clone() * beta2 as f32)?
+                    + (g.clone().square()? * (1.0 - beta2 as f32))?)?;
+                let m_hat = (next_m.clone() * scale_m as f32)?;
+                let v_hat = (next_v.clone() * scale_v as f32)?;
+                let next_theta = (theta.as_tensor().clone() * (1f32 - lr_lambda as f32))?;
+                let adjusted_grad = (m_hat / (v_hat.sqrt()? + self.params.eps as f32)?)?;
+                let next_theta = (next_theta - (adjusted_grad.clone() * lr as f32)?)?;
+
+                #[cfg(feature = "debug")]
+                let next_m = next_m.resolve_debug()?;
+                #[cfg(feature = "debug")]
+                let next_v = next_v.resolve_debug()?;
+                #[cfg(feature = "debug")]
+                let next_theta = next_theta.resolve_debug()?;
+
+                #[cfg(not(feature = "debug"))]
+                let next_m = next_m.resolve_deferred()?;
+                #[cfg(not(feature = "debug"))]
+                let next_v = next_v.resolve_deferred()?;
+                #[cfg(not(feature = "debug"))]
+                let next_theta = next_theta.resolve_deferred()?;
+
+                theta.set(next_theta.clone())?;
+                m.set(next_m.clone())?;
+                v.set(next_v.clone())?;
+            }
+        }
+        Ok(())
+    }
+}
+
+impl AdamW {
+    pub fn new_lr(vars: Vec<(Option<String>, Var)>, learning_rate: f64) -> anyhow::Result<Self> {
+        let params = ParamsAdamW {
+            lr: learning_rate,
+            ..ParamsAdamW::default()
+        };
+        Self::new(vars, params)
+    }
+
+    pub fn params(&self) -> &ParamsAdamW {
+        &self.params
+    }
+
+    pub fn set_params(&mut self, params: ParamsAdamW) {
+        self.params = params;
+    }
+}
---
+++ crates/ratchet-nn/tests/optim.rs
@@ -0,0 +1,111 @@
+use ratchet::{
+    shape,
+    test_utils::{to_vec0_round, to_vec1_round},
+    Device, DeviceRequest, Tensor, Var,
+};
+use ratchet_nn::{AdamW, Linear, Module, Optimizer, ParamsAdamW};
+
+thread_local! {
+    static GPU_DEVICE: Device = Device::request_device(DeviceRequest::GPU).unwrap();
+}
+
+#[test]
+fn adamw_linear_regression() -> anyhow::Result<()> {
+    let device = GPU_DEVICE.with(|d| d.clone());
+    let w_gen = Tensor::from_data(vec![3f32, 1.], shape![1, 2], Device::CPU).to(&device)?;
+    let b_gen = Tensor::from_data(vec![-2f32], shape![1, 1], Device::CPU).to(&device)?;
+    let gen = Linear::new(w_gen, Some(b_gen));
+    let sample_xs = Tensor::from_data(
+        vec![2f32, 1., 7., 4., -4., 12., 5., 8.],
+        shape![4, 2],
+        Device::CPU,
+    );
+    let sample_xs = sample_xs.to(&device)?;
+    let sample_ys = gen.schedule(sample_xs.clone())?;
+
+    // Now use backprop to run a linear regression between samples and get the coefficients back.
+    let w = Var::from_tensor(
+        &Tensor::from_data(vec![0f32, 0.], shape![1, 2], Device::CPU).to(&device)?,
+    )?;
+    // let b = Var::from_data(vec![0f32], shape![1], Device::CPU);
+    let b =
+        Var::from_tensor(&Tensor::from_data(vec![0f32], shape![1, 1], Device::CPU).to(&device)?)?;
+    let params = ParamsAdamW {
+        lr: 0.1,
+        ..Default::default()
+    };
+    let mut opt = AdamW::new(
+        vec![
+            (Some(String::from("b")), b.clone()),
+            (Some(String::from("w")), w.clone()),
+        ],
+        params,
+    )?;
+    let lin = Linear::new(w.as_tensor().clone(), Some(b.as_tensor().clone()));
+    for _step in 0..100 {
+        let ys = lin.schedule(sample_xs.clone())?;
+        let loss = ys.sub(sample_ys.clone())?.square()?.sum(&[0])?;
+        // ratchet::plot::render_to_file(&loss, "forward-pre-schedule.svg").unwrap();
+        let mut grads = opt.schedule_backward_step(&loss)?;
+        // ratchet::plot::render_to_file(&loss, "forward-post-schedule.svg").unwrap();
+        let loss_cpu = loss.clone().resolve()?.to(&Device::CPU)?;
+        let loss_vec = loss_cpu.to_vec::<f32>()?;
+        println!("loss: {:?}", loss_vec[0]);
+        let b = b.as_tensor().to(&Device::CPU)?;
+        let w = w.as_tensor().to(&Device::CPU)?;
+        println!("b: {:?}, w: {:?}", b.to_vec::<f32>(), w.to_vec::<f32>());
+        opt.backward_step(&mut grads, device.try_gpu()?)?;
+    }
+    let b = b.as_tensor().to(&Device::CPU)?;
+    let w = w.as_tensor().to(&Device::CPU)?;
+    println!("b: {:?}, w: {:?}", b.to_vec::<f32>(), w.to_vec::<f32>());
+    assert_eq!(to_vec0_round(&b, 4)?, 0.7872);
+    assert_eq!(to_vec1_round(&w, 4)?, &[2.7257, 0.7097]);
+    Ok(())
+}
+
+#[test]
+fn test_intermediate() -> anyhow::Result<()> {
+    let device = GPU_DEVICE.with(|d| d.clone());
+    let w_gen = Tensor::from_data(vec![3f32, 1.], shape![1, 2], Device::CPU).to(&device)?;
+    let b_gen = Tensor::from_data(vec![-2f32], shape![1, 1], Device::CPU).to(&device)?;
+    let gen = Linear::new(w_gen.clone(), Some(b_gen.clone()));
+    let sample_xs = Tensor::from_data(
+        vec![2f32, 1., 7., 4., -4., 12., 5., 8.],
+        shape![4, 2],
+        Device::CPU,
+    );
+    let sample_xs = sample_xs.to(&device)?;
+    let sample_ys = gen.schedule(sample_xs.clone())?.resolve()?;
+
+    // Now use backprop to run a linear regression between samples and get the coefficients back.
+    let w = Var::from_tensor(
+        &Tensor::from_data(vec![0f32, 0.], shape![1, 2], Device::CPU).to(&device)?,
+    )?;
+    // let b = Var::from_data(vec![0f32], shape![1], Device::CPU);
+    let b =
+        Var::from_tensor(&Tensor::from_data(vec![0f32], shape![1, 1], Device::CPU).to(&device)?)?;
+    let lin = Linear::new(w.as_tensor().clone(), Some(b.as_tensor().clone()));
+
+    let ys = lin.schedule(sample_xs.clone())?;
+    let loss = ys
+        .sub(sample_ys.clone())?
+        .square()?
+        // .sum_last_dim(0)?
+        .resolve()?;
+
+    // Print loss
+    println!("loss: {:?}", loss.to(&Device::CPU)?.to_vec::<f32>()?);
+
+    let gen_grads = loss.backward()?;
+    for (i, (_id, g)) in gen_grads.iter().enumerate() {
+        let g_clone = g.clone().resolve()?;
+        println!(
+            "gen_grads[{}]: (op {:?}) {:?}",
+            i,
+            g_clone.clone().op().name(),
+            g_clone.resolve()?.to(&Device::CPU)?.to_vec::<f32>()?
+        );
+    }
+    Ok(())
+}
