c0891c5fce810c06de864386755b9ae716c91784
Vin Howe <24789592+vinhowe@users.noreply.github.com>
2025-02-11 14:06:53 -0700

    LazyTensor: add FxHasher to crate

--- crates/ratchet-core/src/lib.rs
+++ crates/ratchet-core/src/lib.rs
@@ -32,7 +32,7 @@ pub use ndarray_ext::*;
 pub use op::*;
 pub use ops::*;
 pub use quant::*;
-use rustc_hash::{FxHashMap, FxHashSet};
+use rustc_hash::{FxHashMap, FxHashSet, FxHasher};
 pub use shape::*;
 pub use storage::*;
 pub use strides::*;
@@ -49,6 +49,7 @@ pub type DRVec<T> = SmallVec<[T; 8]>; //Double RVec
 pub type RawGPUBuffer = wgpu::Buffer;
 pub type HashMap<K, V> = FxHashMap<K, V>;
 pub type HashSet<T> = FxHashSet<T>;
+pub type Hasher = FxHasher;
 
 //https://github.com/sonos/tract/blob/main/data/src/macros.rs#L2
 #[macro_export]
