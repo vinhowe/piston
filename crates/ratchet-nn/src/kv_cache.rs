f427703cc020f0938a4fdecce915fdeca2947697
Vin Howe <24789592+vinhowe@users.noreply.github.com>
2025-03-05 20:51:08 -0700

    Flip attention mask (for fun)

--- crates/ratchet-nn/src/kv_cache.rs
+++ crates/ratchet-nn/src/kv_cache.rs
@@ -106,7 +106,7 @@ impl KVCache {
             log::debug!("Creating mask for {:?}", t);
             log::debug!("masks: {:?}", self.masks);
             let ones = Tensor::ones::<f32>(&shape![t, t], &self.device);
-            let mask = ones.triu(Some(1))?;
+            let mask = ones.tril(Some(0))?;
             self.masks.insert(t, mask.clone());
             Ok(mask)
         }
