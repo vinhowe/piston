ba7a8f7162d31943d4733cef333b9a1facf39e02
Vin Howe <24789592+vinhowe@users.noreply.github.com>
2025-03-09 01:43:27 -0700

    Remove unused TensorId import

--- crates/ratchet-web-train/src/model.rs
+++ crates/ratchet-web-train/src/model.rs
@@ -2,8 +2,7 @@ use anyhow::Result;
 use async_trait::async_trait;
 use js_sys::Function;
 use ratchet::{
-    reset_scope_context, shape, DType, Device, DeviceRequest, GradStore, StepLog, Tensor, TensorId,
-    Var,
+    reset_scope_context, shape, DType, Device, DeviceRequest, GradStore, StepLog, Tensor, Var,
 };
 use ratchet_models::gpt2::generate;
 use ratchet_models::gpt2::{Config, GPT2Input, PositionalEncoding};
