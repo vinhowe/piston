9059dfa82603c69d64ca403dc315a9f5b02f57f2
Vin Howe <24789592+vinhowe@users.noreply.github.com>
2025-03-09 01:32:29 -0700

    Add test log utils for web train tests

--- crates/ratchet-web-train/src/lib.rs
+++ crates/ratchet-web-train/src/lib.rs
@@ -1,2 +1,4 @@
 #![cfg(target_arch = "wasm32")]
 mod model;
+#[cfg(test)]
+mod test_utils;
---
+++ crates/ratchet-web-train/src/test_utils.rs
@@ -0,0 +1,21 @@
+pub(crate) fn log_init() {
+    console_error_panic_hook::set_once();
+    let logger = fern::Dispatch::new()
+        .format(|out, message, record| {
+            out.finish(format_args!(
+                "{}[{}][{}] {}",
+                chrono::Local::now().format("[%Y-%m-%d][%H:%M:%S]"),
+                record.target(),
+                record.level(),
+                message
+            ))
+        })
+        .level_for("tokenizers", log::LevelFilter::Off)
+        .level(log::LevelFilter::Info)
+        .chain(fern::Output::call(console_log::log))
+        .apply();
+    match logger {
+        Ok(_) => log::info!("Logging initialized."),
+        Err(error) => eprintln!("Error initializing logging: {:?}", error),
+    }
+}
