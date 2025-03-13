729b427d66fe338c48afee43b84bd7cd9676aaaf
Vin Howe <24789592+vinhowe@users.noreply.github.com>
2025-03-12 19:40:02 -0600

    Move logging setup to lib

--- crates/ratchet-web-train/src/lib.rs
+++ crates/ratchet-web-train/src/lib.rs
@@ -2,3 +2,28 @@
 mod model;
 #[cfg(test)]
 mod test_utils;
+
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen(start)]
+pub fn start() {
+    console_error_panic_hook::set_once();
+    let logger = fern::Dispatch::new()
+        .format(|out, message, record| {
+            out.finish(format_args!(
+                "{}[{}][{}] {}",
+                chrono::Local::now().format("[%Y-%m-%d][%H:%M:%S]"),
+                record.target(),
+                record.level(),
+                message
+            ))
+        })
+        .level_for("tokenizers", log::LevelFilter::Off)
+        .level(log::LevelFilter::Info)
+        .chain(fern::Output::call(console_log::log))
+        .apply();
+    match logger {
+        Ok(_) => log::info!("Logging initialized."),
+        Err(error) => eprintln!("Error initializing logging: {:?}", error),
+    }
+}
