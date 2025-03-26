4e0dce2e2a1af2e3e5d470d878764911b7702cec
Vin Howe <24789592+vinhowe@users.noreply.github.com>
2025-03-25 18:01:35 -0600

    Switch order of where_cond to match pytorch

--- crates/piston-models/src/gpt2/attn.rs
+++ crates/piston-models/src/gpt2/attn.rs
@@ -187,6 +187,6 @@ impl Module for GPT2SelfAttention {
 fn masked_fill(on_false: &Tensor, mask: &Tensor, on_true: f32) -> anyhow::Result<Tensor> {
     let shape = mask.shape();
     let on_true = Tensor::full(shape, on_true, on_false.device(), false)?;
-    let m = mask.clone().where_cond(on_true, on_false.clone())?;
+    let m = on_true.clone().r#where(mask, on_false.clone())?;
     Ok(m)
 }
--- crates/piston-nn/src/util.rs
+++ crates/piston-nn/src/util.rs
@@ -17,9 +17,9 @@ pub fn clip_grad_norm(vars: Vec<Tensor>, max_norm: f32, device: &Device) -> anyh
 
     let clip_coef = (max_norm / (total_norm.clone() + 1e-6)?)?;
     let ones_max = Tensor::ones::<f32, _>(1, device, false)?;
-    let clip_coef = (clip_coef.clone().lt(ones_max.clone()))?
-        .where_cond(clip_coef.clone(), ones_max)?
-        .detach();
+    let clip_coef = clip_coef
+        .clone()
+        .where_cond(clip_coef.clone().lt(ones_max.clone()), ones_max)?;
 
     for var in vars.iter() {
         var.set_grad(var.grad().unwrap().mul(clip_coef.clone())?);
