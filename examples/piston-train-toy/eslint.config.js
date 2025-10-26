6b40a20a92950a740cdb21d6952903f1d8d26743
Vin Howe <24789592+vinhowe@users.noreply.github.com>
2025-10-26 17:11:04 -0600

    Move to eslint defineConfig

--- examples/piston-train-toy/eslint.config.js
+++ examples/piston-train-toy/eslint.config.js
@@ -2,12 +2,13 @@ import prettier from 'eslint-config-prettier';
 import js from '@eslint/js';
 import { includeIgnoreFile } from '@eslint/compat';
 import svelte from 'eslint-plugin-svelte';
+import { defineConfig } from 'eslint/config';
 import globals from 'globals';
 import { fileURLToPath } from 'node:url';
 import ts from 'typescript-eslint';
 const gitignorePath = fileURLToPath(new URL('./.gitignore', import.meta.url));
 
-export default ts.config(
+export default defineConfig(
 	includeIgnoreFile(gitignorePath),
 	js.configs.recommended,
 	...ts.configs.recommended,
