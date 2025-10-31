e068028fc5ec9ceba826f0404db993c36621e82d
Vin Howe <24789592+vinhowe@users.noreply.github.com>
2025-10-31 14:51:53 -0600

    Update config for vitest v4

--- packages/web/vitest.config.ts
+++ packages/web/vitest.config.ts
@@ -1,3 +1,4 @@
+import { playwright } from "@vitest/browser-playwright";
 import tsconfigPaths from "vite-tsconfig-paths";
 import { defineConfig } from "vitest/config";
 
@@ -12,7 +13,7 @@ export default defineConfig({
           setupFiles: ["./vitest-client-setup.ts"],
           browser: {
             enabled: true,
-            provider: "playwright",
+            provider: playwright(),
             instances: [
               {
                 browser: "chromium",
