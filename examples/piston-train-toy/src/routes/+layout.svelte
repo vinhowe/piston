6eadddf709ea7871fb0cdf83acda22e1fee19eea
Vin Howe <24789592+vinhowe@users.noreply.github.com>
2025-11-20 13:40:55 -0700

    Person structured data

--- examples/piston-train-toy/src/routes/+layout.svelte
+++ examples/piston-train-toy/src/routes/+layout.svelte
@@ -66,8 +66,27 @@
 			"operatingSystem": "Any",
 			"creator": {
 				"@type": "Person",
-				"name": "Vin Howe",
-				"sameAs": ["https://vin.how", "https://x.com/vinhowe"]
+				"name": "Thomas Vincent Howe",
+				"alternateName": "Vin Howe",
+				"url": "https://vin.how",
+				"jobTitle": "Graduate Student",
+				"worksFor": {
+					"@type": "Organization",
+					"name": "Brigham Young University",
+					"url": "https://www.byu.edu"
+				},
+				"alumniOf": {
+					"@type": "CollegeOrUniversity",
+					"name": "Brigham Young University",
+					"url": "https://www.byu.edu"
+				},
+				"sameAs": [
+					"https://vin.how",
+					"https://www.linkedin.com/in/tvinhowe",
+					"https://x.com/vinhowe",
+					"https://github.com/vinhowe",
+					"https://scholar.google.com/citations?user=nOym4IEAAAAJ"
+				]
 			}
 		}
 	</script>
